291d0cf6643e38749504dea59ea9a05546f15ccc
Nadina <nadinaalonati@gmail.com>
2023-12-03 23:37:26 -0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const URL = ""
+
+document.getElementById('formulario').addEventListener('submit', function (event) {
+    event.preventDefault();
+}
+
+    var formData = new FormData();
+    formData.append('codigo', document.getElementById('codigo').value);
+    formData.append('descripcion', document.getElementById('descripcion').value);
+    formData.append('cantidad', document.getElementById('cantidad').value);
+    formData.append('precio', document.getElementById('precio').value);
+    formData.append('imagen', document.getElementById('imagen').files[0]);
+    formData.append('proveedor', document.getElementById('proveedor').value);
+
+
+    fetch(URL + 'productos',{
+        method: 'POST',
+        body: formData
+    })
