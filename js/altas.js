e41d89323337cd1604a40fde4a0c1044af526ab3
Nadina <nadinaalonati@gmail.com>
2023-12-03 23:55:39 -0300

    Altas completo

---
+++ js/altas.js
@@ -0,0 +1,43 @@
+const URL = "https://g13.pythonanywhere.com/productos"
+
+document.getElementById('formulario').addEventListener('submit', function (event) {
+    event.preventDefault();
+})
+
+var formData = new FormData();
+formData.append('codigo', document.getElementById('codigo').value);
+formData.append('descripcion', document.getElementById('descripcion').value);
+formData.append('cantidad', document.getElementById('cantidad').value);
+formData.append('precio', document.getElementById('precio').value);
+formData.append('imagen', document.getElementById('imagen').files[0]);
+formData.append('proveedor', document.getElementById('proveedor').value);
+
+
+fetch(URL + 'productos', {
+    method: 'POST',
+    body: formData
+
+    .then((function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Error al agregar producto.');
+        }
+    })
+
+        .then(function () {
+            alert('Producto agregado Correctamente!');
+        })
+    ).catch(function (error) {
+        alert('Error al agregar producto.');
+        console.error('Error', error);
+    })
+    .finally(function () {
+        document.getElementById('codigo').value = "";
+        document.getElementById('descripcion').value = "";
+        document.getElementById('cantidad').value = "";
+        document.getElementById('precio').value = "";
+        document.getElementById('imagenProducto').value = "";
+        document.getElementById('proveedorProducto').value = "";
+    })
+})
