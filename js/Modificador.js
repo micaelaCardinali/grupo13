53a073b45da7bd52ff7c8a6287a30c3a8f8594ba
MattLudu <142941569+MattLudu@users.noreply.github.com>
2023-12-05 02:06:58 -0300

    modificacion

--- js/Modificador.js
+++ js/Modificador.js
@@ -1,6 +1,5 @@
-// Importa la versión de producción de Vue.js
 import { createApp } from 'https://unpkg.com/vue@next/dist/vue.esm-browser.js';
-// importamos el archivo que contiene los componentes a utilizar en nuestra aplicación
+
 const URL = "https://g13.pythonanywhere.com/";
 
 const app = createApp({
@@ -19,83 +18,71 @@ const app = createApp({
     },
 
     methods: {
-        obtenerProducto() {
-            fetch(URL + 'productos/' + this.codigo)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
+        async obtenerProducto() {
+            try {
+                const response = await fetch(URL + 'productos/' + this.codigo);
+
+                if (!response.ok) {
                     throw new Error('Error al obtener datos del Producto');
                 }
-            })
-            .then(data => {
-                return data;
-            })
-                .then(data => {
-                    this.descripcion = data.descripcion;
-                    this.cantidad = data.cantidad;
-                    this.precio = data.precio;
-                    this.proveedor = data.proveedor;
-                    this.imagen_url = data.imagen_url;
-                    this.mostrarDatosProducto = true;
-                })
-                .catch(error => {
-                    console.log(error);
-                    alert('Hubo un error al obtener los datos del producto. Por favor, inténtelo de nuevo.');
-                });
-        },        
-        descargarImagen(url) {
-            const img = new Image();
-            img.crossOrigin = 'Anonymous';
-            img.onload = () => {
-                this.imagen_url = url;
-            };
-            img.src = url;
+
+                const data = await response.json();
+                this.descripcion = data.descripcion;
+                this.cantidad = data.cantidad;
+                this.precio = data.precio;
+                this.proveedor = data.proveedor;
+                this.imagen_url = data.imagen_url;
+                this.mostrarDatosProducto = true;
+            } catch (error) {
+                console.log(error);
+                alert('Hubo un error al obtener los datos del producto. Por favor, inténtelo de nuevo.');
+            }
         },
+
         seleccionarImagen(event) {
             const file = event.target.files[0];
             this.imagenSeleccionada = file;
             this.imagenUrlTemp = URL.createObjectURL(file);
-        },        
-        guardarCambios() {
-            console.log('Guardando cambios...');
-            if (this.imagenSeleccionada && this.imagenSeleccionada.size > MAX_FILE_SIZE) {
-                alert('El tamaño del archivo de imagen es demasiado grande. Por favor, seleccione un archivo más pequeño.');
-                return;
-            }
-        
-            const formData = new FormData();
-            formData.append('codigo', this.codigo);
-            formData.append('descripcion', this.descripcion);
-            formData.append('cantidad', this.cantidad);
-            formData.append('proveedor', this.proveedor);
-            formData.append('precio', this.precio);
-        
-            if (this.imagenSeleccionada) {
-                formData.append('imagen', this.imagenSeleccionada, this.imagenSeleccionada.name);
-            }
-        
-            fetch(URL + 'productos/' + this.codigo, {
-                method: 'PUT',
-                body: formData,
-            })
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error('Error al guardar los cambios');
-                    }
-                })
-                .then(data => {
-                    alert('Producto actualizado correctamente.');
-                    this.limpiarFormulario();
-                })
-                .catch(error => {
-                    console.error('Error', error);
-                    alert('Hubo un error al actualizar el producto. Por favor, inténtelo de nuevo.');
-                });
         },
-        
+
+        async guardarCambios() {
+            try {
+                console.log('Guardando cambios...');
+
+                if (this.imagenSeleccionada && this.imagenSeleccionada.size > MAX_FILE_SIZE) {
+                    alert('El tamaño del archivo de imagen es demasiado grande. Por favor, seleccione un archivo más pequeño.');
+                    return;
+                }
+
+                const formData = new FormData();
+                formData.append('codigo', this.codigo);
+                formData.append('descripcion', this.descripcion);
+                formData.append('cantidad', this.cantidad);
+                formData.append('proveedor', this.proveedor);
+                formData.append('precio', this.precio);
+
+                if (this.imagenSeleccionada) {
+                    formData.append('imagen', this.imagenSeleccionada, this.imagenSeleccionada.name);
+                }
+
+                const response = await fetch(URL + 'productos/' + this.codigo, {
+                    method: 'PUT',
+                    body: formData,
+                });
+
+                if (!response.ok) {
+                    throw new Error('Error al guardar los cambios');
+                }
+
+                const data = await response.json();
+                alert('Producto actualizado correctamente.');
+                this.limpiarFormulario();
+            } catch (error) {
+                console.error('Error al guardar los cambios:', error);
+                alert('Hubo un error al actualizar el producto. Por favor, inténtelo de nuevo.');
+            }
+        },
+
         limpiarFormulario() {
             this.codigo = '';
             this.descripcion = '';
