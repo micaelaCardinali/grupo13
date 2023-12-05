bdc78c2dff43c9a01bb4cf49a30927344a1f33d4
MattLudu <142941569+MattLudu@users.noreply.github.com>
2023-12-05 20:20:52 -0300

    cambios en eliminar

--- js/eliminar.js
+++ js/eliminar.js
@@ -1,41 +1,73 @@
-const URL = "https://g13.pythonanywhere.com/"
-const app = Vue.createApp({
+const URL = "https://g13.pythonanywhere.com/";
+
+new Vue({
+    el: '#app',
     data() {
-        return{
-            productos: []
-        }
+        return {
+            productos: [],
+            rutaBaseImagen: "https://www.pythonanywhere.com/user/G13/files/home/G13/mysite/static/img/"
+        };
+    },
+    mounted() {
+        this.obtenerProductos();
     },
     methods: {
         obtenerProductos() {
             fetch(URL + 'productos')
                 .then(response => {
-                    if (response.ok) {return response.json(); }
-                })
-                .then(data=> {
-                    this.productos = data;
-                })
-                .catch (error => {
-                    console.log('Error',error);
-                    alert('Error al obtener productos.');
-                });
-        },
-        eliminarProducto(codigo){
-            if (confirm('¿Estas Seguro?')) {
-                fetch(URL +`productos/${codigo}`, {method: 'DELETE'})
-                .then(response =>{
-                    if(response.ok){
-                        this.productos = this.productos.filter(producto => producto.codigo !== codigo);
-                        alert('Producto Eliminado')
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Error al obtener productos.');
                     }
                 })
+                .then(data => {
+                    this.productos = data;
+                    this.mostrarRutasEnConsola();
+                })
                 .catch(error => {
-                    alert(error.message);
+                    alert('Error al obtener productos.');
+                    console.error('Error:', error);
                 });
+        },
+        mostrarRutasEnConsola() {
+            for (let producto of this.productos) {
+                const rutaCompleta = this.rutaBaseImagen + producto.imagen_url;
+                console.log('Ruta de la imagen:', rutaCompleta);
+            }
+        },
+        eliminarProducto(codigo) {
+            if (confirm('¿Estás seguro?')) {
+                fetch(URL + `productos/${codigo}`, { method: 'DELETE' })
+                    .then(response => {
+                        if (response.ok) {
+                            this.productos = this.productos.filter(producto => producto.codigo !== codigo);
+                            alert('Producto eliminado');
+                        } else {
+                            throw new Error('Error al eliminar el producto.');
+                        }
+                    })
+                    .catch(error => {
+                        alert(error.message);
+                    });
             }
         }
     },
-    mounted(){
-        this.obtenerProductos();
-    }
+    template: `
+        <div>
+            <div v-for="producto in productos" :key="producto.codigo" class="product-card">
+                <img :src="rutaBaseImagen + producto.imagen_url" :alt="producto.descripcion" class="product-img">
+                <div class="product-info">
+                    <div>
+                        <p>\${{ producto.precio }}</p>
+                        <p>{{ producto.descripcion }}</p>
+                    </div>
+                    <figure>
+                        <img src="./assets/icons/agregarCarrito.png" alt="">
+                    </figure>
+                    <button @click="eliminarProducto(producto.codigo)">Eliminar</button>
+                </div>
+            </div>
+        </div>
+    `
 });
-app.mount('body')
