8df589fac7fdd1cbe40fa98d000a63ad063e2688
Nadina <nadinaalonati@gmail.com>
2023-12-07 20:45:19 -0300

    prueba1

--- js/altas.js
+++ js/altas.js
@@ -8,33 +8,33 @@ document.getElementById('formulario').addEventListener('submit', function (event
     formData.append('descripcion', document.getElementById('descripcion').value);
     formData.append('cantidad', document.getElementById('cantidad').value);
     formData.append('precio', document.getElementById('precio').value);
-    formData.append('imagen', document.getElementById('imagen').files[0]);
-    formData.append('proveedor', document.getElementById('proveedor').value);
+    formData.append('imagen', document.getElementById('imagenProducto').files[0]);
+    formData.append('proveedor', document.getElementById('proveedorProducto').value);
 
     fetch(URL + '/productos', {
-    method: 'POST',
-    body: formData
+        method: 'POST',
+        body: formData
     })
-    .then(function (response) {
-        if (response.ok) {
+        .then(function (response) {
+            if (response.ok) {
                 return response.json();
-        } else {
+            } else {
                 throw new Error('Error al agregar producto.');
-        }
-    })
-    .then(function(){
-        alert('Producto agregado Correctamente!');
-    })
-    .catch(function (error) {
-        alert('Error al agregar producto.');
-        console.error('Error', error);
-    })
-    .finally(function () {
-    document.getElementById('codigo').value = "";
-    document.getElementById('descripcion').value = "";
-    document.getElementById('cantidad').value = "";
-    document.getElementById('precio').value = "";
-    document.getElementById('imagen').value = "";
-    document.getElementById('proveedor').value = "";
-    });
+            }
+        })
+        .then(function () {
+            alert('Producto agregado Correctamente!');
+        })
+        .catch(function (error) {
+            alert('Error al agregar producto.');
+            console.error('Error', error);
+        })
+        .finally(function () {
+            document.getElementById('codigo').value = "";
+            document.getElementById('descripcion').value = "";
+            document.getElementById('cantidad').value = "";
+            document.getElementById('precio').value = "";
+            document.getElementById('imagen').value = "";
+            document.getElementById('proveedor').value = "";
+        });
 })
